33a63dfec185bdd24b0d102a97a4abedf919a2fc
ValJeansEvilBread <64847510+ValJeansEvilBread@users.noreply.github.com>
2020-08-06 17:01:02 -0400

    commit one for this repository

--- src/main/java/com/tts/Admirablety/controller/AuthorizationController.java
+++ src/main/java/com/tts/Admirablety/controller/AuthorizationController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tts.Admirablety.model.User;
 import com.tts.Admirablety.service.UserService;
@@ -25,7 +27,13 @@ public class AuthorizationController {
 
     @GetMapping(value="/login")
     public String login(){
-        return "login";
+        return "index";
+    }
+    
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+      return "index";
     }
     
     @GetMapping(value="/register")
